f7913f1cca1c4311c16be9fa3df1fedb03f9e9c4
Akis Vasiliou <akis@liferide.co.uk>
2020-02-11 18:27:57 +0000

    added console.log

--- src/pages/file-upload.js
+++ src/pages/file-upload.js
@@ -1,41 +1,42 @@
-import React from 'react'
-import { navigate } from 'gatsby-link'
-import Layout from '../layout'
+import React from "react";
+import { navigate } from "gatsby-link";
+import Layout from "../layout";
 
 function encode(data) {
-  const formData = new FormData()
+  const formData = new FormData();
 
   for (const key of Object.keys(data)) {
-    formData.append(key, data[key])
+    formData.append(key, data[key]);
   }
 
-  return formData
+  return formData;
 }
 
 export default function Contact() {
-  const [state, setState] = React.useState({})
+  const [state, setState] = React.useState({});
 
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
-  const handleAttachment = (e) => {
-    setState({ ...state, [e.target.name]: e.target.files[0] })
-  }
+  const handleAttachment = e => {
+    console.log("e: ", e);
+    setState({ ...state, [e.target.name]: e.target.files[0] });
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
       body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
+        "form-name": form.getAttribute("name"),
+        ...state
+      })
     })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
   return (
     <Layout>
@@ -52,7 +53,8 @@ export default function Contact() {
         <input type="hidden" name="form-name" value="file-upload" />
         <p hidden>
           <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+            Don’t fill this out:{" "}
+            <input name="bot-field" onChange={handleChange} />
           </label>
         </p>
         <p>
@@ -72,8 +74,10 @@ export default function Contact() {
         <p>
           <button type="submit">Send</button>
         </p>
-        <p>Note: multiple file uploads are not supported by Netlify at this time.</p>
+        <p>
+          Note: multiple file uploads are not supported by Netlify at this time.
+        </p>
       </form>
     </Layout>
-  )
+  );
 }
